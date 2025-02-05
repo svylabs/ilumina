4e7656a2c48373db4cd37f927301614c75a0687c
svylabs <sg@svylabs.com>
2025-02-05 16:15:07 +0100

    Compiling binaries

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@ import { Actor } from "./actor";
 import { Action } from "./action";
 import { Account } from "./account";
 import { PRNG } from "./prng";
+import { Agent, Environment } from "flocc";
 export type { SnapshotProvider, Account, Web3RunnerOptions, RunContext };
-export { Actor, Action, Runner, PRNG };
+export { Actor, Action, Runner, PRNG, Agent, Environment };
 //# sourceMappingURL=index.d.ts.map
