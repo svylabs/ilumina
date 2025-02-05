18ca0ee377f737430eac2dfb5701f2cbbe9d753c
svylabs <sg@svylabs.com>
2025-02-05 16:00:39 +0100

    Adding scaffold

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { Actor } from "./actor";
 import { Action } from "./action";
 import { Account } from "./account";
 import { PRNG } from "./prng";
+import {Agent, Environment} from "flocc";
 
 export type { SnapshotProvider, Account, Web3RunnerOptions, RunContext };
 
-export { Actor, Action, Runner, PRNG };
+export { Actor, Action, Runner, PRNG, Agent, Environment };
